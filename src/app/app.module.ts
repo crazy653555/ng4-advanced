cec11151d53f42c6a13982460c86bf2641524e9a
eason <crazy653555@gmail.com>
2017-10-29 13:57:16 +0800

    08 練習建立 Charts 子路由並設定必要的 ngOnInit 事件初始化元件 jQuery 程式

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { FlotComponent } from './charts/flot/flot.component';
+import { RadialComponent } from './charts/radial/radial.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    CardsComponent
+    CardsComponent,
+    FlotComponent,
+    RadialComponent
+
   ],
   imports: [
     BrowserModule,
