420598be5a41f2b482b4f3e32e686e6ad822744d
eason <crazy653555@gmail.com>
2017-10-29 14:39:00 +0800

    09 練習透過注入 Router 服務元件達成透過程式碼進行 navigate 導覽到不同路由

--- src/app/fallback-route.ts
+++ src/app/fallback-route.ts
@@ -1,6 +1,6 @@
-import { Route } from "@angular/router";
+import { Route } from '@angular/router';
 
 export const fallbackRoute: Route = {
   path: '**',
-  redirectTo: '/page2', pathMatch: 'full'
+  redirectTo: '/dashboard', pathMatch: 'full'
 };
