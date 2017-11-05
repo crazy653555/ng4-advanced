369eac27f36d338d1d3b7514da70888ec14e3941
eason <crazy653555@gmail.com>
2017-11-05 15:06:47 +0800

    21 練習 FormGroup 與 FormArray 的應用技巧

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form2',
@@ -14,9 +14,33 @@ export class Form2Component implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      title:['hello',[Validators.required,Validators.minLength(2)]],
-      subtitle:["eason"]
+      title: ['hello', [Validators.required, Validators.minLength(2)]],
+      subtitle: ["eason"],
+      people: this.fb.array([
+        this.fb.group({
+          name: '',
+          tel: '',
+          email: ''
+        }),
+        this.fb.group({
+          name: this.fb.control(''),
+          tel: '',
+          email: ''
+        })
+
+      ])
     });
+  }
+
+  addPerson() {
+    let people = this.form.get('people') as FormArray;
+    people.push(
+      this.fb.group({
+        name: this.fb.control(''),
+        tel: '',
+        email: ''
+      })
+    );
   }
 
 }
